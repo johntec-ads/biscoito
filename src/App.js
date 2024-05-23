b023a24ca2b10659b5e74aa1f744dd491005862f
johntec-ads <johntec.ads@gmail.com>
2024-05-23 06:55:02 -0300

    upProject

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from  'react' ;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hello World John</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    };
+  }
+
+
+  render() {
+    return(
+      <div>
+        <img src={require('./assects/biscoito.png')}  alt=''></img> 
+        <Botao/>       
+      </div>
+    )   
+    
+  }
 }
 
-export default App;
+class Botao extends Component {
+  render() {
+    return(
+      <div>
+        <button>Abrir biscoito</button>
+      </div>
+
+    )
+  }
+}
+
+
+
+export default App ;
