33ed003098ef8906081289fe11341a7d3646b9eb
johntec-ads <johntec.ads@gmail.com>
2024-05-28 06:59:40 -0300

    projectOne

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+
 import React, { Component } from 'react';
 
 class App extends Component {
 
   constructor( props ) {
     super( props );
-    this.state = {
+    this.state = {      
+      frases : ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.']
+     }
 
+      
+     this.quebrarBiscoito = this.quebrarBiscoito.bind(this)
     }
 
+  quebrarBiscoito() {
+    alert('Botão funcionou !!!')
   }
 
   render () {
@@ -15,6 +27,7 @@ class App extends Component {
       <div>
         <img src={ require( './assects/biscoito.png' ) } alt='' />
         <Botao />
+        <h3> Frase número 1 aleatória... </h3>
       </div>
 
     )
@@ -25,7 +38,7 @@ class Botao extends Component {
   render() {
     return (
       <div>
-        <button>Quebra biscoito</button>   
+        <button onClick={this.quebrarBiscoito} >Abrir biscoito</button>   
       </div>
     )
   }
