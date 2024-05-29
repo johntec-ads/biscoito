a1dceec79c1312cf6e5f98decd1166e6f7e77ca9
John Marcelo <johntec.ads@gmail.com>
2024-05-29 15:45:12 -0300

    projectBiscoito

--- src/App.js
+++ src/App.js
@@ -5,40 +5,50 @@ class App extends Component {
 
   constructor( props ) {
     super( props );
-    this.state = {      
-      frases : ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
-      'O riso é a menor distância entre duas pessoas.', 
+    this.state = {
+      textoFrase : ''
+     }
+
+   this.frases = [ 'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
       'Deixe de lado as preocupações e seja feliz.',
       'Realize o óbvio, pense no improvável e conquiste o impossível.',
       'Acredite em milagres, mas não dependa deles.',
-      'A maior barreira para o sucesso é o medo do fracasso.']
-     }
+      'A maior barreira para o sucesso é o medo do fracasso.' ]
 
-      
-     this.quebrarBiscoito = this.quebrarBiscoito.bind(this)
-    }
+    this.quebrarBiscoito = this.quebrarBiscoito.bind( this )
 
-  quebrarBiscoito() {
-    alert('Botão funcionou !!!')
+  }
+
+  quebrarBiscoito () {
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = this.frases[numeroAleatorio];
+    this.setState(state)
   }
 
   render () {
-    return (
+    return (/* Component principal */
       <div>
+        {/* elemento src com atribuição de require */}
         <img src={ require( './assects/biscoito.png' ) } alt='' />
-        <Botao />
-        <h3> Frase número 1 aleatória... </h3>
+        <Botao nome='abrirBiscoito' acaoBtn={ this.quebrarBiscoito } />
+        <h3> {this.state.textoFrase} </h3>
+
       </div>
 
     )
   }
 }
 
+/* Component Botao */
 class Botao extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <button onClick={this.quebrarBiscoito} >Abrir biscoito</button>   
+        {/* elemento button , atribuir onClick */}
+        <button onClick={ this.props.acaoBtn }> { this.props.nome } </button>
+
       </div>
     )
   }
