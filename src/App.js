8cbe110ad26dba91e4968482cdb54b6e39d95996
John Marcelo <johntec.ads@gmail.com>
2024-06-03 21:30:58 -0300

    upprojetct

--- src/App.js
+++ src/App.js
@@ -4,15 +4,43 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      frase: '',
     }
-    
-  }
 
+    
+    this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+    'O riso é a menor distância entre duas pessoas.', 
+ 'Deixe de lado as preocupações e seja feliz.',
+ 'Realize o óbvio, pense no improvável e conquiste o impossível.',
+ 'Acredite em milagres, mas não dependa deles.',
+ 'A maior barreira para o sucesso é o medo do fracasso.'];
+    
+ this.geraFrase = this.geraFrase.bind(this);
+}
+
+geraFrase() {
+  let aleatorio = Math.floor(Math)
+
+
+}
+
+  /* Renderização do componente principal */
   render() {
     return(
       <div>
-        <h1>Hello World</h1>
+        <h1>BISCOITO DA SORTE...</h1>
+        <img src={require('./assects/biscoito.png')} alt=''/>
+        <Botao nome='Quebrar Biscoito' acaoBtn={this.geraFrase} />
+      </div>
+    )
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn} > {this.props.nome} </button>
       </div>
     )
   }
